3ca21fc79424d7695919c01ac14428dee9c05cb8
André Miguel <123982689+DreNunes@users.noreply.github.com>
2026-07-31 17:29:14 -0300

    Enhance employee profile with position and salary details
    Added employee position, salary, and employee card generation. Included employee code parsing for department, year code, initials, and last three digits.

--- Exercises/Exercise 2 - Build_an_Employee_Profile_Manager.py
+++ Exercises/Exercise 2 - Build_an_Employee_Profile_Manager.py
@@ -1,4 +1,4 @@
-#In this exercise I'm building an Employee Profile Generator using basic concepts, such as variable creation and concatenation
+#In this exercise I'm building an Employee Profile Generator using basic concepts, such as variable creation and strings operations like concatenation and slicing
 
 first_name = 'John'
 last_name = 'Doe'
@@ -11,3 +11,16 @@ print(employee_info)
 experience_years = 5
 experience_info = 'Experience: ' + str(experience_years) + ' years'
 print(experience_info)
+position = 'Data Analyst'
+salary = 75000
+employee_card = f'Employee: {full_name} | Age: {employee_age} | Position: {position} | Salary: ${salary}'
+print(employee_card)
+employee_code = 'DEV-2026-JD-001'
+department = employee_code[0:3]
+print(department)
+year_code = employee_code[4:8]
+print(year_code)
+initials = employee_code[9:11]
+print(initials)
+last_three = employee_code[-3:]
+print(last_three)
