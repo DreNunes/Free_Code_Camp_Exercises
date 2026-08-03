ecd31c5f01f9e4acbdfa33cfce61188866d66c70
André Miguel <123982689+DreNunes@users.noreply.github.com>
2026-08-03 15:32:28 -0300

    Refactor weather commuting conditions logic

--- Labs/Weather_Travel_Planner.py
+++ Labs/Weather_Travel_Planner.py
@@ -14,3 +14,25 @@ has_car = False
 
 #epresents if the user has an app that allows them to request a ride
 has_ride_share_app = False
+
+# If distance_mi is a falsy value:
+if not distance_mi:
+  print(False)
+# If the distance is less than or equal to 1 mile:
+elif distance_mi <= 1:
+  if not is_raining:
+    print(True)
+  else:
+    print(False)
+# If the distance is greater than 1 mile and less than or equal to 6 miles:
+elif distance_mi <= 6:
+  if has_bike and not is_raining:
+    print(True)
+  else:
+    print(False)
+# If the person has a car or has a ride-share app
+else:
+  if has_car or has_ride_share_app:
+    print(True)
+  else:
+    print(False)
