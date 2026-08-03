71113a811a0acc5b116320584119556a87cf830d
André Miguel <123982689+DreNunes@users.noreply.github.com>
2026-08-03 11:43:35 -0300

    Update Weather_Travel_Planner.py
    First variable declarations

---
+++ Labs/Weather_Travel_Planner.py
@@ -0,0 +1,16 @@
+# With the use of conditional statements, this code will determine wether commuting is possible based on the weather.
+
+#represents the distance to travel in miles
+distance_mi = 0
+
+#represents if the user is currently experiencing rainy weather
+is_raining = False
+
+#represents if the user has a bicycle
+has_bike = False
+
+#represents if the user has a car
+has_car = False
+
+#epresents if the user has an app that allows them to request a ride
+has_ride_share_app = False
