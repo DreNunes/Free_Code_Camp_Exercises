4b62810d85a2acb890910462cfce9e39e1aacdc8
André Miguel <123982689+DreNunes@users.noreply.github.com>
2026-08-11 17:28:44 -0300

    Put the line secret_code += str(len(words[line_index])) in an if statement that checks that there are enough words in the words list

--- Workshops/Exercise 6 - Build_a_PIN_Extractor.py
+++ Workshops/Exercise 6 - Build_a_PIN_Extractor.py
@@ -4,7 +4,8 @@ def pin_extractor(poem):
   lines = poem.split('\n')
   for line_index, line in enumerate(lines):
     words = lines.split()
-    secret_code += str(len(words[line_index]))
+    if len(words) > line_index:
+      secret_code += str(len(words[line_index]))
   return secret_code
 
 poem = '''Stars and the moon
