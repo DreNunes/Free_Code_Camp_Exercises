b6376922c3f73fffd70c816763d9c61119cd1760
André Miguel <123982689+DreNunes@users.noreply.github.com>
2026-08-19 11:27:36 -0300

    Implement Email class constructor and example usage

--- Workshops/Exercise 8 - Build_an_Email_Simulator.py
+++ Workshops/Exercise 8 - Build_an_Email_Simulator.py
@@ -1,3 +1,11 @@
 class Email:
   def __init__(self, sender, receiver, subject, body):
-    pass
+    self.sender = sender
+    self.receiver = receiver
+    self.subject = subject
+    self.body = body
+
+email_obj = Email('alice@example.com', 'bob@example.com', 'Hello', 'Hi Bob!')
+
+print(email_obj.sender)
+print(email_obj.subject)
